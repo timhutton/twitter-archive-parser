8db328512f5e1968e3c9d89d8bd7e67047fefb94
Tim Hutton <tihutt@microsoft.com>
2022-11-09 22:55:38 +0000

    Fix: allow for multiple URLs. Added timestamp.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twitter-archive-parser
 Python code to parse a Twitter archive and output in various ways
+
+Features:
+- Replaces t.co URLs with their original versions
--- parser.py
+++ parser.py
@@ -1,7 +1,6 @@
 import json
 import os
 
-
 def read_twitter_archive(folder):
     with open(os.path.join(folder, 'data', 'tweet.js'), 'r', encoding='utf8') as f:
         data = f.readlines()
@@ -15,18 +14,17 @@ def extract_tweet(tweet):
     body = tweet['full_text']
     # replace t.co URLs with their original versions
     for url in tweet['entities']['urls']:
-        start_char = int(url['indices'][0])
-        end_char = int(url['indices'][1])
-        body = body[:start_char] + url['expanded_url'] + body[end_char:]
-    return body
+        body = body.replace(url['url'], url['expanded_url'])
+    return tweet['created_at'] + '\n' + body
 
 def main():
     json = read_twitter_archive('.')
     tweets_text = [extract_tweet(tweet) for tweet in json]
     all_tweets = '\n\n'.join(tweets_text)
-    with open('output.txt', 'w', encoding='utf-8') as f:
+    output_filename = 'output.txt'
+    with open(output_filename, 'w', encoding='utf-8') as f:
         f.write(all_tweets)
-
+    print('Wrote to', output_filename)
 
 if __name__ == "__main__":
     main()
