e8c59d4ec7e5b849a3728b1785dc6e17ea4ca9e5
Tim Hutton <tihutt@microsoft.com>
2022-11-10 10:14:02 +0000

    Minor: added license header

--- parser.py
+++ parser.py
@@ -1,3 +1,21 @@
+"""
+    twitter-archive-parser - Python code to parse a Twitter archive and output in various ways
+    Copyright (C) 2022  Tim Hutton
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import json
 import os
 
