50c817593d1f22df236f30d10544646ba9bd1457
Tim Hutton <tihutt@microsoft.com>
2022-11-10 00:01:01 +0000

    Insert images as markdown. Add link to original tweet.

--- parser.py
+++ parser.py
@@ -9,22 +9,38 @@ def read_twitter_archive(folder):
         # parse the resulting JSON
         return json.loads(data)
 
-def extract_tweet(tweet):
+def extract_tweet(tweet, username):
     tweet = tweet['tweet']
+    timestamp = tweet['created_at']
     body = tweet['full_text']
+    tweet_id_str = tweet['id_str']
     # replace t.co URLs with their original versions
     for url in tweet['entities']['urls']:
         body = body.replace(url['url'], url['expanded_url'])
-    return tweet['created_at'] + '\n' + body
+    # replace image URLs with markdown image links to local files
+    if 'media' in tweet['entities']:
+        for media in tweet['entities']['media']:
+            original_url = media['url']
+            original_filename = os.path.split(media['media_url'])[1]
+            new_filename = 'tweet_media/' + tweet_id_str + '-' + original_filename
+            markdown = f'![]({new_filename})'
+            body = body.replace(original_url, markdown)
+    # append the original Twitter URL as a link
+    body += f'\n\nOriginally on Twitter: [{timestamp}](https://twitter.com/{username}/status/{tweet_id_str})'
+    return body
 
 def main():
-    json = read_twitter_archive('.')
-    tweets_text = [extract_tweet(tweet) for tweet in json]
-    all_tweets = '\n\n'.join(tweets_text)
+
+    input_folder = '.'
+    username = '_tim_hutton_'
     output_filename = 'output.txt'
+
+    json = read_twitter_archive(input_folder)
+    tweets_text = [extract_tweet(tweet, username) for tweet in json]
+    all_tweets = '\n--\n'.join(tweets_text)
     with open(output_filename, 'w', encoding='utf-8') as f:
         f.write(all_tweets)
-    print('Wrote to', output_filename)
+    print(f'Parsed {len(json)} tweets. Wrote to', output_filename)
 
 if __name__ == "__main__":
     main()
